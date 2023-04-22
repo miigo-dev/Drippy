d774c250e2b4344a3d25c0fc30b6a7c459188531
miigo-dev <miigo.mnl@gmail.com>
2023-04-22 18:39:06 +0800

    tapping on sign in button takes user to login

--- app/src/main/java/com/example/drippy/MainActivity.java
+++ app/src/main/java/com/example/drippy/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.drippy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvClickableText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvClickableText = findViewById(R.id.textViewSignIn);
+        tvClickableText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
