0648580668745e6e38ad18dece9eef4d57e40c5d
miigo-dev <miigo.mnl@gmail.com>
2023-05-28 01:03:26 +0800

    gawa ni palabs

---
+++ app/src/main/java/com/example/drippy/Shipping_Fee.java
@@ -0,0 +1,14 @@
+package com.example.drippy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shipping_Fee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shipping_fee);
+    }
+}
