4d0a43f4f34d99f6da62a3d0cd9d56e386695fd8
miigo-dev <miigo.mnl@gmail.com>
2023-05-31 19:12:31 +0800

    added outfit suggestion activity

---
+++ app/src/main/java/com/example/drippy/OutfitSuggestion.java
@@ -0,0 +1,14 @@
+package com.example.drippy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OutfitSuggestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outfit_suggestion);
+    }
+}
