357f77dbe19cf8b43c6d240edc2234b16d7bcad8
miigo-dev <miigo.mnl@gmail.com>
2023-05-31 19:09:43 +0800

    updated

--- app/src/main/java/com/example/drippy/Homepage.java
+++ app/src/main/java/com/example/drippy/Homepage.java
@@ -2,14 +2,23 @@ package com.example.drippy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
+    Button btnGetStarted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+        btnGetStarted.setOnClickListener(view -> {
+            Intent intent = new Intent(Homepage.this, Wardrobe.class);
+            startActivity(intent);
+        });
     }
 
     public void onBackPressed() {
