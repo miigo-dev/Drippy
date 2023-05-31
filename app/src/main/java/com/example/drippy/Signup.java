5dfb5d1d2055d4108df8978422e07f8527f02b4a
miigo-dev <miigo.mnl@gmail.com>
2023-05-31 14:15:57 +0800

    updated button: sizes, margins, color

--- app/src/main/java/com/example/drippy/Signup.java
+++ app/src/main/java/com/example/drippy/Signup.java
@@ -4,38 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
-    TextView tvClickableText;
-
-    Button btnClickable;
+    Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        tvClickableText = findViewById(R.id.txtViewSignIn);
-        tvClickableText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Signup.this, Login.class);
-                startActivity(intent);
-            }
+        btnCreate = findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(view -> {
+            Intent intent = new Intent(Signup.this, Homepage.class);
+            startActivity(intent);
         });
-
-        btnClickable = findViewById(R.id.btnCreate);
-        btnClickable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Signup.this, Login.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
