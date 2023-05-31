cb13d8a16844cacc6ddc534b0f22d6f0c77a8429
miigo-dev <miigo.mnl@gmail.com>
2023-05-31 18:56:45 +0800

    signup + signin now takes you to respective activities (homepage, signin)

--- app/src/main/java/com/example/drippy/Login.java
+++ app/src/main/java/com/example/drippy/Login.java
@@ -1,13 +1,29 @@
 package com.example.drippy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(Login.this, Homepage.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 }
