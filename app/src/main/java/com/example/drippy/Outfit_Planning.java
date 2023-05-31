7b73228fd72a848a6f61e812c8f1390e25c08de3
isaac <isaacerrolmariano123@gmail.com>
2023-06-01 00:18:02 +0800

    op mema

--- app/src/main/java/com/example/drippy/Outfit_Planning.java
+++ app/src/main/java/com/example/drippy/Outfit_Planning.java
@@ -2,13 +2,21 @@ package com.example.drippy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 public class Outfit_Planning extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_planning);
+
+
     }
 }
