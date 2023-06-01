994b0b178aba5f3aa946133894f74b1f7d064202
Angelo Palabay <gelpalabay05@gmail.com>
2023-06-02 04:19:06 +0800

    Seller

--- app/src/main/java/com/example/drippy/Add_Product.java
+++ app/src/main/java/com/example/drippy/Add_Product.java
@@ -1,16 +1,22 @@
 package com.example.drippy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Add_Product extends AppCompatActivity {
 
     private ImageButton categoryBtn;
     private ImageButton shippingFeeBtn;
     private ImageButton back1;
+    private ImageButton saveBtn;
+    private ImageButton imageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,23 @@ public class Add_Product extends AppCompatActivity {
 
         ImageButton categoryBtn = findViewById(R.id.categoryBtn);
         ImageButton shippingFeeBtn = findViewById(R.id.shippingFeeBtn);
+        ImageButton saveBtn = findViewById(R.id.saveBtn);
+
+
         ImageButton back1 = findViewById(R.id.back1);
+
+        imageButton = findViewById(R.id.uploadBtn);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera, 100);
+
+
+
+            }
+        });
 
         categoryBtn.setOnClickListener(view -> {
             Intent intent = new Intent(Add_Product.this, Variation.class);
@@ -35,6 +57,11 @@ public class Add_Product extends AppCompatActivity {
             Intent intent = new Intent(Add_Product.this, Sell.class);
             startActivity(intent);
         });
-
+        saveBtn.setOnClickListener(view -> {
+            Toast.makeText(Add_Product.this, "Item Added.",
+                    Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Add_Product.this, Sell.class);
+            startActivity(intent);
+        });
     }
 }
