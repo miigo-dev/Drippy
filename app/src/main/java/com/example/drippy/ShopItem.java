1f92269bae39f1dc87b691e756a83db8ea58e19c
miigo-dev <miigo.mnl@gmail.com>
2023-06-03 05:08:12 +0800

    implemented shop_item

---
+++ app/src/main/java/com/example/drippy/ShopItem.java
@@ -0,0 +1,14 @@
+package com.example.drippy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShopItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_item);
+    }
+}
