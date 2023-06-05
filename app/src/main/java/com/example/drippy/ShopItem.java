56e163f6fd897bddb88e04a64222b4ed01722b77
Angelo Palabay <gelpalabay05@gmail.com>
2023-06-06 01:17:21 +0800

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/drippy/ShopItem.java
+++ app/src/main/java/com/example/drippy/ShopItem.java
@@ -1,14 +1,24 @@
 package com.example.drippy;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ShopItem extends AppCompatActivity {
+
+    Button buyNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_item);
+
+        buyNow = findViewById(R.id.btnBuyNow);
+        buyNow.setOnClickListener(view -> {
+            Intent intent = new Intent(ShopItem.this, shop_checkout.class );
+            startActivity(intent);
+        });
     }
 }
